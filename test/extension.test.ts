468c98b6d0baaf1be30c2e71d47d111acc99452c
kellen <841449084@qq.com>
2026-04-29 15:59:37 +0800

    refactor: 套装

--- test/extension.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from 'vitest'
-
-test('弹出窗口应正确显示', async () => {
-  const popupPage = await browser.getPopupPage()
-  const title = await popupPage.title()
-  expect(title).toBeTruthy()
-})
